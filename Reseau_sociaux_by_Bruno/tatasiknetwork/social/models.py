67d6c3c648ffd08588304489532ff09599b565a8
tsikyFitiavana <tsikybr@gmail.com>
2023-04-04 07:42:08 +0300

    post publication sans file pour le moment

---
+++ Reseau_sociaux_by_Bruno/tatasiknetwork/social/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+	body = models.TextField()
+	created_on = models.DateTimeField(default=timezone.now)
+	author = models.ForeignKey(User, on_delete=models.CASCADE)
