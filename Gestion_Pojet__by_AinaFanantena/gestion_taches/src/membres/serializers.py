871e96f1955a68250c38447e016d644121013fa0
Aina16 <106653623+Aina16@users.noreply.github.com>
2023-04-08 15:08:41 +0300

    Delete Gestion_Pojet__by_AinaFanantena/gestion_taches directory

--- Gestion_Pojet__by_AinaFanantena/gestion_taches/src/membres/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import Membres
-class MembreSerializer(serializers.ModelSerializer):
-    class Meta:
-       model = Membres
-       fields = '__all__'
--- Gestion_Pojet__by_AinaFanantena/gestion_taches/src/taches/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from django.contrib.auth.models import User
-from .models import Taches
-class TachesSerializer(serializers.ModelSerializer):
-    class Meta:
-       model = Taches
-       fields = '__all__'
